6043949e555ba91318faa1ac1a9a12e0bf45ef5e
DESKTOP-13RQSSJ\HP <Ezechukwuchirimmadu@gmail.com>
2023-07-26 22:05:32 +0100

    feat: add location lat long category and description

--- config/plugins.js
+++ config/plugins.js
@@ -15,5 +15,33 @@ module.exports = ({ env }) => ({
         },
       },
     },
+
+    "fuzzy-search": {
+      enabled: true,
+      config: {
+        contentTypes: [
+          {
+            uid: "api::event.event",
+            modelName: "event",
+            transliterate: true,
+            fuzzysortOptions: {
+              characterLimit: 300,
+              threshold: -600,
+              limit: 10,
+              keys: [
+                {
+                  name: "name",
+                  weight: 100,
+                },
+                {
+                  name: "description",
+                  weight: -100,
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
     // ...
   });
